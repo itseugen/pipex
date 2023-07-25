bbc8fd6aeb38a8185d978940cfcf756033d96a6e
eweiberl <eweiberl@student.42heilbronn.de>
2023-07-25 17:17:03 +0200

    put code into new funcitons

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 14:19:47 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/07/25 15:43:03 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/07/25 17:15:51 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,36 +19,14 @@ void	leaks_check(void)
 	white();
 }
 
-// int	main(int argc, char *argv[])
-// {
-// 	t_pipe	pipe;
-
-// 	atexit(leaks_check);
-// 	if (argc < 5)
-// 		return (0);
-// 	if (argc > 5) /*Remove for bonus*/
-// 		return (0);
-// 	pipe.fd_in = open(argv[1], O_RDONLY);
-// 	pipe.fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-// 	if (pipe.fd_in == -1 || pipe.fd_out == -1)
-// 		return (perror("open"), clean_exit(pipe), 1);
-// 	if (dup2(pipe.fd_in, STDIN_FILENO) == -1)
-// 		return (perror("dup2 fail"), clean_exit(pipe), 1);
-// 	if (dup2(pipe.fd_out, STDOUT_FILENO) == -1)
-// 		return (perror("dup2 fail"), clean_exit(pipe), 1);
-// 	execute_cmd(argv[2]);
-// 	close (pipe.fd_in);
-// 	close (pipe.fd_out);
-// 	return (0);
-// }
-
 int	main(int argc, char *argv[])
 {
 	t_pipe	pipe_x;
 	int		pipe_fd[2];
 	pid_t	pro_id;
+	pid_t	pro_id2;
 
-	atexit(leaks_check);
+	//atexit(leaks_check);
 	if (argc < 5)
 		return (0);
 	if (argc > 5) /*Remove for bonus*/
@@ -63,29 +41,22 @@ int	main(int argc, char *argv[])
 	if (pro_id == -1)
 		return (perror("fork fail"), 1);
 	if (pro_id == 0) //!Child procress
-	{
-		close(pipe_fd[0]);
-		pipe_x.fd_in = open(argv[1], O_RDONLY);
-		if (dup2(pipe_x.fd_in, STDIN_FILENO) == -1)
-			return (perror("dup2 fail"), close(pipe_fd[1]), clean_exit(pipe_x), 1);
-		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-			return (perror("dup2 fail"), close(pipe_fd[1]), clean_exit(pipe_x), 1);
-		execute_cmd(argv[2]);
-		close(pipe_fd[1]);
-	}
+		first_child(pipe_fd, pipe_x, argv);
 	else //!Parent process
 	{
-		close(pipe_fd[1]);
-		wait(NULL);
-		if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
-			return (perror("dup2 fail"), close(pipe_fd[0]), clean_exit(pipe_x), 1);
-		if (dup2(pipe_x.fd_out, STDOUT_FILENO) == -1)
-			return (perror("dup2 fail"), close(pipe_fd[0]), clean_exit(pipe_x), 1);
-		execute_cmd(argv[3]);
-		close(pipe_fd[0]);
+		pro_id2 = fork();
+		if (pro_id2 == -1)
+			return (perror("fork fail"), 1);
+		if (pro_id2 == 0)
+			last_child(pipe_fd, pipe_x, argv);
 	}
 	close(pipe_x.fd_in);
 	close(pipe_x.fd_out);
+	if (pro_id != 0 && pro_id2 != 0)
+	{
+		wait(NULL);
+		system("leaks pipex");
+	}
 	return (0);
 }
 
@@ -97,7 +68,7 @@ void	clean_exit(t_pipe pipe)
 		close (pipe.fd_in);
 	if (pipe.fd_out != -1)
 		close (pipe.fd_out);
-	exit(1);
+	exit(0);
 }
 
 	// int n;
@@ -116,3 +87,51 @@ void	clean_exit(t_pipe pipe)
     // else
     //     perror("read");
     // exit(0);
+/*
+int	main(int argc, char *argv[])
+{
+	t_pipe	pipe_x;
+	int		pipe_fd[2];
+	pid_t	pro_id;
+
+	atexit(leaks_check);
+	if (argc < 5)
+		return (0);
+	if (argc > 5)
+		return (0);
+	pipe_x.fd_in = open(argv[1], O_RDONLY);
+	pipe_x.fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipe_x.fd_in == -1 || pipe_x.fd_out == -1)
+		return (perror("open"), clean_exit(pipe_x), 1);
+	if (pipe(pipe_fd) == -1)
+		return (perror("pipe fail"), 1);
+	pro_id = fork();
+	if (pro_id == -1)
+		return (perror("fork fail"), 1);
+	if (pro_id == 0) //!Child procress
+	{
+		close(pipe_fd[0]);
+		pipe_x.fd_in = open(argv[1], O_RDONLY);
+		if (dup2(pipe_x.fd_in, STDIN_FILENO) == -1)
+			return (perror("dup2 fail"), close(pipe_fd[1]), clean_exit(pipe_x), 1);
+		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+			return (perror("dup2 fail"), close(pipe_fd[1]), clean_exit(pipe_x), 1);
+		close(pipe_fd[1]);
+		execute_cmd(argv[2]);
+	}
+	else //!Parent process
+	{
+		close(pipe_fd[1]);
+		wait(NULL);
+		if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
+			return (perror("dup2 fail"), close(pipe_fd[0]), clean_exit(pipe_x), 1);
+		if (dup2(pipe_x.fd_out, STDOUT_FILENO) == -1)
+			return (perror("dup2 fail"), close(pipe_fd[0]), clean_exit(pipe_x), 1);
+		execute_cmd(argv[3]);
+		close(pipe_fd[0]);
+	}
+	close(pipe_x.fd_in);
+	close(pipe_x.fd_out);
+	return (0);
+}
+*/
